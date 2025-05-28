2312d99723f608bdb95009a25d39854bce70a564
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-05-28 11:06:16 +0200

    new nextconfig

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
-  // Prevent API routes from being analyzed during build
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  poweredByHeader: false,
-  // Don't try to optimize API routes
-  swcMinify: true,
+  // Completely skip build-time route analysis
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
+  // Don't collect page data during build
+  staticPageGenerationTimeout: 1000,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  }
 }
 
 module.exports = nextConfig
